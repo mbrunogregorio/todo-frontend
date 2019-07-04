6a4edbb944c546f8ae9048e1ccfff84633e06dfa
Bruno Gregorio <brunomoises@gmail.com>
2019-07-04 17:05:48 -0400

    First commit

---
+++ src/main/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/font-awesome/css/font-awesome.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div><i className="fa fa-user"></i> Tao tome entao</div>
+    </div>
+  );
+}
+
+export default App;
